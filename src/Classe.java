5cbbf7ed35f26be45abe7691da4c8947e191fcc6
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-02-18 20:25:15 -0300

    sasasa

--- src/Classe.java
+++ src/Classe.java
@@ -1,10 +1,17 @@
-
 public class Classe {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, world!");
-
+		
+		
+		// Interio maximo = 1254224
+		int int_maximo = 1254000;	
+		System.out.println(int_maximo);
+		
+		
+		
+		
 	}
 
 }
